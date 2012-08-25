4a00190c374e4a4722a38e3d4d219098d55932b0
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2012-08-25 09:46:34 +0900

    test_example_match.rb : remove last comma of hash parameter to pass test under 1.8.7

--- test/test_example_match.rb
+++ test/test_example_match.rb
@@ -2717,7 +2717,7 @@ class TestExampleMatch < Test::Unit::TestCase
     data = [
             [ 1, 2, 3, 4,  5 ],
             [ 2, 4, 6, 8,  10 ],
-            [ 3, 6, 9, 12, 15 ],
+            [ 3, 6, 9, 12, 15 ]
 
            ]
 
@@ -2725,12 +2725,12 @@ class TestExampleMatch < Test::Unit::TestCase
 
     chart.add_series(
                      :categories => '=Sheet1!$A$1:$A$5',
-                     :values     => '=Sheet1!$B$1:$B$5',
+                     :values     => '=Sheet1!$B$1:$B$5'
                     )
 
     chart.add_series(
                      :categories => '=Sheet1!$A$1:$A$5',
-                     :values     => '=Sheet1!$C$1:$C$5',
+                     :values     => '=Sheet1!$C$1:$C$5'
                     )
 
     chart.set_x_axis(:minor_unit => 1, :major_unit => 3)
