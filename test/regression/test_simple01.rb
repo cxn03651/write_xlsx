5ffd52d391ffe13a2be0150eae6e9bc94259e5a9
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2012-09-08 06:41:23 +0900

    regression/test_simple01, 02.rb added and pass

---
+++ test/regression/test_simple01.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestSimple01 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_simple01
+    @xlsx = 'simple01.xlsx'
+    workbook  = WriteXLSX.new(@xlsx)
+    worksheet = workbook.add_worksheet
+
+    worksheet.write('A1', 'Hello')
+    worksheet.write('A2', 123)
+
+    workbook.close
+    compare_xlsx_for_regression(File.join(@regression_output, @xlsx), @xlsx)
+  end
+end
---
+++ test/regression/test_simple02.rb
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestSimple02 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_simple02
+    @xlsx = 'simple02.xlsx'
+    workbook  = WriteXLSX.new(@xlsx)
+    worksheet1 = workbook.add_worksheet
+    worksheet2 = workbook.add_worksheet('Data Sheet')
+    worksheet3 = workbook.add_worksheet
+
+    bold = workbook.add_format(:bold => 1)
+
+    worksheet1.write('A1', 'Foo')
+    worksheet1.write('A2', 123)
+
+    worksheet3.write('B2', 'Foo')
+    worksheet3.write('B3', 'Bar', bold)
+    worksheet3.write('C4', 234)
+
+    workbook.close
+    compare_xlsx_for_regression(File.join(@regression_output, @xlsx), @xlsx)
+  end
+end
