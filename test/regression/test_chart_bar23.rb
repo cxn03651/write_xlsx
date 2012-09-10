8b09b31960cf609b9fc520b791a61a3423095329
cxn03651 <cxn03651@msj.biglobe.ne.jp>
2012-09-10 18:03:42 +0900

    regression/test_chart_bar22, 23.rb added and pass

---
+++ test/regression/test_chart_bar23.rb
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRegressionChartBar23 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_chart_bar23
+    @xlsx = 'chart_bar23.xlsx'
+    workbook   = WriteXLSX.new(@xlsx)
+    worksheet  = workbook.add_worksheet
+    chart      = workbook.add_chart(:type => 'bar', :embedded => 1)
+
+    # For testing, copy the randomly generated axis ids in the target xlsx file.
+    chart.instance_variable_set(:@axis_ids, [43706240, 43727104])
+
+    headers = ['Series 1', 'Series 2', 'Series 3']
+    data = [
+            [ 'Category 1', 'Category 2', 'Category 3', 'Category 4' ],
+            [ 4.3,          2.5,          3.5,          4.5 ],
+            [ 2.4,          4.5,          1.8,          2.8 ],
+            [ 2,            2,            3,            5 ]
+           ]
+
+    worksheet.set_column('A:D', 11)
+
+    worksheet.write('B1', headers)
+    worksheet.write('A2', data)
+
+    chart.add_series(
+                     :categories      => '=Sheet1!$A$2:$A$5',
+                     :values          => '=Sheet1!$B$2:$B$5'
+                     )
+
+    chart.add_series(
+                     :categories      => '=Sheet1!$A$2:$A$5',
+                     :values          => '=Sheet1!$C$2:$C$5'
+                     )
+
+    chart.add_series(
+                     :categories      => '=Sheet1!$A$2:$A$5',
+                     :values          => '=Sheet1!$D$2:$D$5'
+                     )
+
+    worksheet.insert_chart('E9', chart)
+
+    workbook.close
+    compare_xlsx_for_regression(File.join(@regression_output, @xlsx),
+                                @xlsx,
+                                nil,
+                                {
+                                  'xl/charts/chart1.xml' => ['<c:pageMargins']
+                                }
+                                )
+  end
+end
+
