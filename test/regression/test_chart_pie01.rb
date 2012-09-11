5428ceb615f918db1516ab6e1a0cf5ea60a993f1
cxn03651 <cxn03651@msj.biglobe.ne.jp>
2012-09-11 12:10:37 +0900

    regression/test_chart_pie01.rb added and pass

---
+++ test/regression/test_chart_pie01.rb
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRegressionChartPie01 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_chart_pie01
+    @xlsx = 'chart_pie01.xlsx'
+    workbook    = WriteXLSX.new(@xlsx)
+    worksheet   = workbook.add_worksheet
+    chart       = workbook.add_chart(:type => 'pie', :embedded => 1)
+
+    data = [
+            [ 2,  4,  6],
+            [60, 30, 10]
+           ]
+
+    worksheet.write('A1', data)
+
+    chart.add_series(
+                     :categories => '=Sheet1!$A$1:$A$3',
+                     :values     => '=Sheet1!$B$1:$B$3'
+                     )
+
+    worksheet.insert_chart('E9', chart)
+
+    workbook.close
+    compare_xlsx_for_regression(File.join(@regression_output, @xlsx), @xlsx)
+  end
+end
