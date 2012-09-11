9633e82cc18aa62dae1921aa53bfd8bb774a60e2
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2012-09-12 07:33:05 +0900

    regression/test_repeat01..05.rb added and pass

---
+++ test/regression/test_repeat02.rb
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRepeat02 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_repeat02
+    @xlsx = 'repeat02.xlsx'
+    workbook  = WriteXLSX.new(@xlsx)
+    worksheet = workbook.add_worksheet
+
+    worksheet.repeat_columns('A:A')
+
+    worksheet.write('A1', 'Foo')
+
+    workbook.close
+    compare_xlsx_for_regression(
+                                File.join(@regression_output, @xlsx),
+                                @xlsx,
+                                [
+                                 'xl/printerSettings/printerSettings1.bin',
+                                 'xl/worksheets/_rels/sheet1.xml.rels'
+                                ],
+                                {
+                                 '[Content_Types].xml'      => ['<Default Extension="bin"'],
+                                 'xl/worksheets/sheet1.xml' => ['<pageMargins', '<pageSetup']
+                                }
+                                )
+  end
+end
---
+++ test/regression/test_repeat05.rb
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRepeat05 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_repeat05
+    @xlsx = 'repeat05.xlsx'
+    workbook   = WriteXLSX.new(@xlsx)
+    worksheet1 = workbook.add_worksheet
+    worksheet2 = workbook.add_worksheet
+    worksheet3 = workbook.add_worksheet
+
+    worksheet1.repeat_rows(0)
+    worksheet3.repeat_rows(2, 3)
+    worksheet3.repeat_columns('B:F')
+
+    worksheet1.write('A1', 'Foo')
+
+    workbook.close
+    compare_xlsx_for_regression(
+                                File.join(@regression_output, @xlsx),
+                                @xlsx,
+                                [
+                                 'xl/printerSettings/printerSettings1.bin',
+                                 'xl/printerSettings/printerSettings2.bin',
+                                 'xl/worksheets/_rels/sheet1.xml.rels',
+                                 'xl/worksheets/_rels/sheet3.xml.rels'
+                                ],
+                                {
+                                 '[Content_Types].xml'      => ['<Default Extension="bin"'],
+                                 'xl/worksheets/sheet1.xml' => ['<pageMargins', '<pageSetup'],
+                                 'xl/worksheets/sheet3.xml' => ['<pageMargins', '<pageSetup']
+                                }
+                                )
+  end
+end
