34dc5c25c17d999d2ae87266e3635bd0677bb51e
cxn03651 <cxn03651@msj.biglobe.ne.jp>
2012-10-03 17:56:57 +0900

    test_example_match.rb : test_shape1..4 added. 1..3 pass, but 4 failure.

--- test/test_example_match.rb
+++ test/test_example_match.rb
@@ -2864,4 +2864,363 @@ class TestExampleMatch < Test::Unit::TestCase
     East      Grape     6000      February
 EOS
   end
+
+  def test_shape1
+    @xlsx = 'shape1.xlsx'
+    workbook  = WriteXLSX.new(@xlsx)
+    worksheet = workbook.add_worksheet
+
+    # Add a circle, with centered text.
+    ellipse = workbook.add_shape(
+      :type   => 'ellipse',
+      :text   => "Hello\nWorld",
+      :width  => 60,
+      :height => 60
+                                 )
+
+    worksheet.insert_shape('A1', ellipse, 50, 50)
+
+    # Add a plus sign.
+    plus = workbook.add_shape(:type => 'plus', :width => 20, :height => 20)
+    worksheet.insert_shape('D8', plus)
+
+    workbook.close
+    compare_xlsx(File.join(@perl_output, @xlsx), @xlsx)
+  end
+
+  def test_shape2
+    @xlsx = 'shape2.xlsx'
+    workbook  = WriteXLSX.new(@xlsx)
+    worksheet = workbook.add_worksheet
+
+    worksheet.hide_gridlines(2)
+
+    plain = workbook.add_shape(
+      :type   => 'smileyFace',
+      :text   => "Plain",
+      :width  => 100,
+      :height => 100
+                                 )
+
+    bbformat = workbook.add_format(
+      :color => 'red',
+      :font  => 'Lucida Calligraphy'
+                                   )
+
+    bbformat.set_bold
+    bbformat.set_underline
+    bbformat.set_italic
+
+    decor = workbook.add_shape(
+      :type        => 'smileyFace',
+      :text        => 'Decorated',
+      :rotation    => 45,
+      :width       => 200,
+      :height      => 100,
+      :format      => bbformat,
+      :line_type   => 'sysDot',
+      :line_weight => 3,
+      :fill        => 'FFFF00',
+      :line        => '3366FF'
+                               )
+
+    worksheet.insert_shape('A1', plain,  50, 50)
+    worksheet.insert_shape('A1', decor, 250, 50)
+
+    workbook.close
+    compare_xlsx(File.join(@perl_output, @xlsx), @xlsx)
+  end
+
+  def test_shape3
+    @xlsx = 'shape3.xlsx'
+    workbook  = WriteXLSX.new(@xlsx)
+    worksheet = workbook.add_worksheet
+
+    normal = workbook.add_shape(
+      :name   => 'chip',
+      :type   => 'diamond',
+      :text   => 'Normal',
+      :width  => 100,
+      :height => 100
+                                 )
+
+    worksheet.insert_shape('A1', normal, 50, 50)
+    normal[:text] = 'Scaled 3w x 2h'
+    normal[:name] = 'Hope'
+    worksheet.insert_shape('A1', normal, 250, 50, 3, 2)
+
+    workbook.close
+    compare_xlsx(File.join(@perl_output, @xlsx), @xlsx)
+  end
+
+  def test_shape4
+    @xlsx = 'shape4.xlsx'
+    workbook  = WriteXLSX.new(@xlsx)
+    worksheet = workbook.add_worksheet
+    worksheet.hide_gridlines(2)
+
+    type = 'rect'
+    shape = workbook.add_shape(
+      :type   => type,
+      :width  => 90,
+      :height => 90
+                                 )
+
+    (1..10).each do |n|
+      # Change the last 5 rectangles to stars. Previously
+      # inserted shapes stay as rectangles.
+      type = 'star5' if n == 6
+      shape[:type] = type
+      shape[:text] = "#{type} #{n}"
+      worksheet.insert_shape('A1', shape, n * 100, 50)
+    end
+
+    stencil = workbook.add_shape(
+      :stencil => 1,     # The default.
+      :width   => 90,
+      :height  => 90,
+      :text    => 'started as a box'
+                                 )
+    worksheet.insert_shape('A1', stencil, 100, 150)
+
+    stencil[:stencil] = 0
+    worksheet.insert_shape('A1', stencil, 200, 150)
+    worksheet.insert_shape('A1', stencil, 300, 150)
+
+    # Ooopa! Changed my mind.
+    # Change the rectangle to an ellipse (circle),
+    # for the last two shapes.
+    stencil[:type] = 'ellipse'
+    stencil[:text] = 'Now its a circle'
+
+    workbook.close
+    compare_xlsx(File.join(@perl_output, @xlsx), @xlsx)
+  end
+
+  def later_test_shape_all
+    @xlsx = 'shape_all.xlsx'
+    workbook = WriteXLSX.new(@xlsx)
+    worksheet  = nil
+    last_sheet = ''
+    row        = 0
+
+    shapes_list.each_line do |line|
+      line.chomp
+      next unless line =~ /^\w/    # Skip blank lines and comments.
+
+      sheet, name = line.split(/\t/)
+      if last_sheet != sheet
+        worksheet = workbook.add_worksheet(sheet)
+        row       = 2
+      end
+      last_sheet = sheet
+      shape      = workbook.add_shape(
+                                  :type   => name,
+                                  :text   => name,
+                                  :width  => 90,
+                                  :height => 90
+                                  )
+
+      # Connectors can not have labels, so write the connector name in the cell
+      # to the left.
+      worksheet.write(row, 0, name ) if sheet == 'Connector'
+      worksheet.insert_shape(row, 2, shape, 0, 0)
+      row += 5
+    end
+
+    workbook.close
+    compare_xlsx(File.join(@perl_output, @xlsx), @xlsx)
+  end
+
+  def shapes_list
+    <<EOS
+Action	actionButtonBackPrevious
+Action	actionButtonBeginning
+Action	actionButtonBlank
+Action	actionButtonDocument
+Action	actionButtonEnd
+Action	actionButtonForwardNext
+Action	actionButtonHelp
+Action	actionButtonHome
+Action	actionButtonInformation
+Action	actionButtonMovie
+Action	actionButtonReturn
+Action	actionButtonSound
+Arrow	bentArrow
+Arrow	bentUpArrow
+Arrow	circularArrow
+Arrow	curvedDownArrow
+Arrow	curvedLeftArrow
+Arrow	curvedRightArrow
+Arrow	curvedUpArrow
+Arrow	downArrow
+Arrow	leftArrow
+Arrow	leftCircularArrow
+Arrow	leftRightArrow
+Arrow	leftRightCircularArrow
+Arrow	leftRightUpArrow
+Arrow	leftUpArrow
+Arrow	notchedRightArrow
+Arrow	quadArrow
+Arrow	rightArrow
+Arrow	stripedRightArrow
+Arrow	swooshArrow
+Arrow	upArrow
+Arrow	upDownArrow
+Arrow	uturnArrow
+Basic	blockArc
+Basic	can
+Basic	chevron
+Basic	cube
+Basic	decagon
+Basic	diamond
+Basic	dodecagon
+Basic	donut
+Basic	ellipse
+Basic	funnel
+Basic	gear6
+Basic	gear9
+Basic	heart
+Basic	heptagon
+Basic	hexagon
+Basic	homePlate
+Basic	lightningBolt
+Basic	line
+Basic	lineInv
+Basic	moon
+Basic	nonIsoscelesTrapezoid
+Basic	noSmoking
+Basic	octagon
+Basic	parallelogram
+Basic	pentagon
+Basic	pie
+Basic	pieWedge
+Basic	plaque
+Basic	rect
+Basic	round1Rect
+Basic	round2DiagRect
+Basic	round2SameRect
+Basic	roundRect
+Basic	rtTriangle
+Basic	smileyFace
+Basic	snip1Rect
+Basic	snip2DiagRect
+Basic	snip2SameRect
+Basic	snipRoundRect
+Basic	star10
+Basic	star12
+Basic	star16
+Basic	star24
+Basic	star32
+Basic	star4
+Basic	star5
+Basic	star6
+Basic	star7
+Basic	star8
+Basic	sun
+Basic	teardrop
+Basic	trapezoid
+Basic	triangle
+Callout	accentBorderCallout1
+Callout	accentBorderCallout2
+Callout	accentBorderCallout3
+Callout	accentCallout1
+Callout	accentCallout2
+Callout	accentCallout3
+Callout	borderCallout1
+Callout	borderCallout2
+Callout	borderCallout3
+Callout	callout1
+Callout	callout2
+Callout	callout3
+Callout	cloudCallout
+Callout	downArrowCallout
+Callout	leftArrowCallout
+Callout	leftRightArrowCallout
+Callout	quadArrowCallout
+Callout	rightArrowCallout
+Callout	upArrowCallout
+Callout	upDownArrowCallout
+Callout	wedgeEllipseCallout
+Callout	wedgeRectCallout
+Callout	wedgeRoundRectCallout
+Chart	chartPlus
+Chart	chartStar
+Chart	chartX
+Connector	bentConnector2
+Connector	bentConnector3
+Connector	bentConnector4
+Connector	bentConnector5
+Connector	curvedConnector2
+Connector	curvedConnector3
+Connector	curvedConnector4
+Connector	curvedConnector5
+Connector	straightConnector1
+FlowChart	flowChartAlternateProcess
+FlowChart	flowChartCollate
+FlowChart	flowChartConnector
+FlowChart	flowChartDecision
+FlowChart	flowChartDelay
+FlowChart	flowChartDisplay
+FlowChart	flowChartDocument
+FlowChart	flowChartExtract
+FlowChart	flowChartInputOutput
+FlowChart	flowChartInternalStorage
+FlowChart	flowChartMagneticDisk
+FlowChart	flowChartMagneticDrum
+FlowChart	flowChartMagneticTape
+FlowChart	flowChartManualInput
+FlowChart	flowChartManualOperation
+FlowChart	flowChartMerge
+FlowChart	flowChartMultidocument
+FlowChart	flowChartOfflineStorage
+FlowChart	flowChartOffpageConnector
+FlowChart	flowChartOnlineStorage
+FlowChart	flowChartOr
+FlowChart	flowChartPredefinedProcess
+FlowChart	flowChartPreparation
+FlowChart	flowChartProcess
+FlowChart	flowChartPunchedCard
+FlowChart	flowChartPunchedTape
+FlowChart	flowChartSort
+FlowChart	flowChartSummingJunction
+FlowChart	flowChartTerminator
+Math	mathDivide
+Math	mathEqual
+Math	mathMinus
+Math	mathMultiply
+Math	mathNotEqual
+Math	mathPlus
+Star_Banner	arc
+Star_Banner	bevel
+Star_Banner	bracePair
+Star_Banner	bracketPair
+Star_Banner	chord
+Star_Banner	cloud
+Star_Banner	corner
+Star_Banner	diagStripe
+Star_Banner	doubleWave
+Star_Banner	ellipseRibbon
+Star_Banner	ellipseRibbon2
+Star_Banner	foldedCorner
+Star_Banner	frame
+Star_Banner	halfFrame
+Star_Banner	horizontalScroll
+Star_Banner	irregularSeal1
+Star_Banner	irregularSeal2
+Star_Banner	leftBrace
+Star_Banner	leftBracket
+Star_Banner	leftRightRibbon
+Star_Banner	plus
+Star_Banner	ribbon
+Star_Banner	ribbon2
+Star_Banner	rightBrace
+Star_Banner	rightBracket
+Star_Banner	verticalScroll
+Star_Banner	wave
+Tabs	cornerTabs
+Tabs	plaqueTabs
+Tabs	squareTabs
+EOS
+  end
 end
