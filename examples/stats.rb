c0f2e047d06b98a84cdd0375ff4c9fbbd9210386
cxn03651 <cxn03651@msj.biglobe.ne.jp>
2012-10-05 10:51:59 +0900

    examples/stats.rb added.

---
+++ examples/stats.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+#######################################################################
+#
+# A Simple example of how to use functions with the WriteXLSX gem.
+#
+# reverse(c), March 2001, John McNamara, jmcnamara@cpan.org
+# convert to ruby by Hideo NAKAMURA, cxn03651@msj.biglobe.ne.jp
+#
+
+require 'rubygems'
+require 'write_xlsx'
+
+# Create a new workbook and add a worksheet
+workbook  = WriteXLSX.new('stats.xlsx')
+worksheet = workbook.add_worksheet('Test data')
+
+# Set the column width for column 1
+worksheet.set_column(0, 0, 20)
+
+# Create a format for the headings
+format = workbook.add_format
+format.set_bold
+
+# Write the sample data
+worksheet.write(0, 0, 'Sample', format)
+worksheet.write(0, 1, 1)
+worksheet.write(0, 2, 2)
+worksheet.write(0, 3, 3)
+worksheet.write(0, 4, 4)
+worksheet.write(0, 5, 5)
+worksheet.write(0, 6, 6)
+worksheet.write(0, 7, 7)
+worksheet.write(0, 8, 8)
+
+worksheet.write(1, 0, 'Length', format)
+worksheet.write(1, 1, 25.4)
+worksheet.write(1, 2, 25.4)
+worksheet.write(1, 3, 24.8)
+worksheet.write(1, 4, 25.0)
+worksheet.write(1, 5, 25.3)
+worksheet.write(1, 6, 24.9)
+worksheet.write(1, 7, 25.2)
+worksheet.write(1, 8, 24.8)
+
+# Write some statistical functions
+worksheet.write(4, 0, 'Count', format)
+worksheet.write(4, 1, '=COUNT(B1:I1)')
+
+worksheet.write(5, 0, 'Sum', format)
+worksheet.write(5, 1, '=SUM(B2:I2)')
+
+worksheet.write(6, 0, 'Average', format)
+worksheet.write(6, 1, '=AVERAGE(B2:I2)')
+
+worksheet.write(7, 0, 'Min', format)
+worksheet.write(7, 1, '=MIN(B2:I2)')
+
+worksheet.write(8, 0, 'Max', format)
+worksheet.write(8, 1, '=MAX(B2:I2)')
+
+worksheet.write(9, 0, 'Standard Deviation', format)
+worksheet.write(9, 1, '=STDEV(B2:I2)')
+
+worksheet.write(10, 0, 'Kurtosis', format)
+worksheet.write(10, 1, '=KURT(B2:I2)')
+
+workbook.close
--- test/test_example_match.rb
+++ test/test_example_match.rb
@@ -2921,6 +2921,45 @@ EOS
     compare_xlsx(File.join(@perl_output, @xlsx), @xlsx)
   end
 
+  def test_regions
+    @xlsx = 'regions.xlsx'
+    workbook  = WriteXLSX.new(@xlsx)
+
+    # Add some worksheets
+    north = workbook.add_worksheet('North')
+    south = workbook.add_worksheet('South')
+    east  = workbook.add_worksheet('East')
+    west  = workbook.add_worksheet('West')
+
+    # Add a Format
+    format = workbook.add_format
+    format.set_bold
+    format.set_color('blue')
+
+    # Add a caption to each worksheet
+    workbook.sheets.each do |worksheet|
+      worksheet.write(0, 0, 'Sales', format)
+    end
+
+    # Write some data
+    north.write(0, 1, 200000)
+    south.write(0, 1, 100000)
+    east.write(0, 1, 150000)
+    west.write(0, 1, 100000)
+
+    # Set the active worksheet
+    south.activate
+
+    # Set the width of the first column
+    south.set_column(0, 0, 20)
+
+    # Set the active cell
+    south.set_selection(0, 1)
+
+    workbook.close
+    compare_xlsx(File.join(@perl_output, @xlsx), @xlsx)
+  end
+
   def test_rich_strings
     @xlsx = 'rich_strings.xlsx'
     workbook  = WriteXLSX.new(@xlsx)
@@ -3529,6 +3568,65 @@ Tabs	squareTabs
 EOS
   end
 
+  def test_stats
+    @xlsx = 'stats.xlsx'
+    workbook = WriteXLSX.new(@xlsx)
+    worksheet = workbook.add_worksheet('Test data')
+
+    # Set the column width for column 1
+    worksheet.set_column(0, 0, 20)
+
+    # Create a format for the headings
+    format = workbook.add_format
+    format.set_bold
+
+    # Write the sample data
+    worksheet.write(0, 0, 'Sample', format)
+    worksheet.write(0, 1, 1)
+    worksheet.write(0, 2, 2)
+    worksheet.write(0, 3, 3)
+    worksheet.write(0, 4, 4)
+    worksheet.write(0, 5, 5)
+    worksheet.write(0, 6, 6)
+    worksheet.write(0, 7, 7)
+    worksheet.write(0, 8, 8)
+
+    worksheet.write(1, 0, 'Length', format)
+    worksheet.write(1, 1, 25.4)
+    worksheet.write(1, 2, 25.4)
+    worksheet.write(1, 3, 24.8)
+    worksheet.write(1, 4, 25.0)
+    worksheet.write(1, 5, 25.3)
+    worksheet.write(1, 6, 24.9)
+    worksheet.write(1, 7, 25.2)
+    worksheet.write(1, 8, 24.8)
+
+    # Write some statistical functions
+    worksheet.write(4, 0, 'Count', format)
+    worksheet.write(4, 1, '=COUNT(B1:I1)')
+
+    worksheet.write(5, 0, 'Sum', format)
+    worksheet.write(5, 1, '=SUM(B2:I2)')
+
+    worksheet.write(6, 0, 'Average', format)
+    worksheet.write(6, 1, '=AVERAGE(B2:I2)')
+
+    worksheet.write(7, 0, 'Min', format)
+    worksheet.write(7, 1, '=MIN(B2:I2)')
+
+    worksheet.write(8, 0, 'Max', format)
+    worksheet.write(8, 1, '=MAX(B2:I2)')
+
+    worksheet.write(9, 0, 'Standard Deviation', format)
+    worksheet.write(9, 1, '=STDEV(B2:I2)')
+
+    worksheet.write(10, 0, 'Kurtosis', format)
+    worksheet.write(10, 1, '=KURT(B2:I2)')
+
+    workbook.close
+    compare_xlsx(File.join(@perl_output, @xlsx), @xlsx)
+  end
+
   def test_tab_colors
     @xlsx = 'tab_colors.xlsx'
     workbook = WriteXLSX.new(@xlsx)
@@ -3547,3 +3645,4 @@ EOS
     compare_xlsx(File.join(@perl_output, @xlsx), @xlsx)
   end
 end
+
