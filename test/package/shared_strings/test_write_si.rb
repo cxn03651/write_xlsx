a114d1797b9fc86c83fca8482bb840c19cae3288
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2012-11-22 15:54:21 +0900

    test_write_si.rb : add setup

--- test/package/shared_strings/test_write_si.rb
+++ test/package/shared_strings/test_write_si.rb
@@ -3,8 +3,11 @@ require 'helper'
 require 'write_xlsx/package/shared_strings'
 
 class TestWriteSi < Test::Unit::TestCase
-  def test_write_si
+  def setup
     @obj = Writexlsx::Package::SharedStrings.new
+  end
+
+  def test_write_si
     @obj.__send__('write_si', 'neptune')
     result = @obj.instance_variable_get(:@writer).string
     expected = '<si><t>neptune</t></si>'
