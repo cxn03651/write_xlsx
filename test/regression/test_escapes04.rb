9e77a77ce1e6b8720cf6cea1bde2665439b370a1
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2013-02-05 09:53:07 +0900

    regression/test_escape04.rb added and pass

---
+++ test/regression/test_escapes04.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRegressionEscapes04 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_chart_escapes04
+    @xlsx = 'escapes04.xlsx'
+    workbook    = WriteXLSX.new(@xlsx)
+    worksheet   = workbook.add_worksheet
+
+    worksheet.write_url('A1', 'http://www.perl.com/?a=1&b=2')
+
+    workbook.close
+    compare_xlsx_for_regression(
+                                File.join(@regression_output, @xlsx),
+                                @xlsx,
+                                nil,
+                                {
+                                  'xl/workbook.xml' => ['<workbookView']
+                                }
+                                )
+  end
+end
