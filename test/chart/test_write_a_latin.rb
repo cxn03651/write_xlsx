6a3e05c03cf1b9fdc783e109e691ccfe64d2daba
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2013-02-05 09:51:14 +0900

    chart/test_write_a_latin.rb added.

---
+++ test/chart/test_write_a_latin.rb
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+require 'helper'
+require 'write_xlsx/chart'
+
+class TestWriteALatin < Test::Unit::TestCase
+  def setup
+    @chart = Writexlsx::Chart.new('Bar')
+  end
+
+  def test_write_a_latin
+    expected = '<a:latin typeface="Arial" pitchFamily="34" charset="0"/>'
+
+    result = @chart.__send__('write_a_latin',
+                             [
+                              'typeface', 'Arial',
+                              'pitchFamily', 34,
+                              'charset', 0
+                             ]
+                         )
+    assert_equal(expected, result)
+  end
+end
