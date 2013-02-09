5e19f8c4247461f4218b46aabc35bff7929f0012
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2013-02-10 08:57:28 +0900

    add tests for 0.57

---
+++ test/regression/test_chart_gridlines06.rb
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestChartGridlines06 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_chart_gridlines06
+    @xlsx = 'chart_gridlines06.xlsx'
+    workbook    =  WriteXLSX.new(@xlsx)
+    worksheet   = workbook.add_worksheet
+    chart       = workbook.add_chart(:type => 'scatter', :embedded => 1)
+
+    # For testing, copy the randomly generated axis ids in the target xls file.
+    chart.instance_variable_set(:@axis_ids, [82812288, 46261376])
+
+    data = [
+            [ 1, 2, 3, 4,  5 ],
+            [ 2, 4, 6, 8,  10 ],
+            [ 3, 6, 9, 12, 15 ]
+           ]
+
+    worksheet.write('A1', data)
+
+    chart.add_series(
+                     :categories => '=Sheet1!$A$1:$A$5',
+                     :values     => '=Sheet1!$B$1:$B$5'
+                     )
+    chart.add_series(
+                     :categories => '=Sheet1!$A$1:$A$5',
+                     :values     => '=Sheet1!$C$1:$C$5'
+                     )
+
+    chart.set_x_axis(
+                     :major_gridlines => { :visible => 1 },
+                     :minor_gridlines => { :visible => 1 }
+                     )
+    chart.set_y_axis(
+                     :major_gridlines => { :visible => 1 },
+                     :minor_gridlines => { :visible => 1 }
+                     )
+
+    worksheet.insert_chart('E9',  chart)
+
+    workbook.close
+    compare_xlsx_for_regression(
+                                File.join(@regression_output, @xlsx),
+                                @xlsx
+                                )
+  end
+end
---
+++ test/regression/test_chart_gridlines09.rb
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestChartGridlines09 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_chart_gridlines09
+    @xlsx = 'chart_gridlines09.xlsx'
+    workbook    =  WriteXLSX.new(@xlsx)
+    worksheet   = workbook.add_worksheet
+    chart       = workbook.add_chart(:type => 'column', :embedded => 1)
+
+    # For testing, copy the randomly generated axis ids in the target xls file.
+    chart.instance_variable_set(:@axis_ids, [48744320, 49566848])
+
+    data = [
+            [ 1, 2, 3, 4,  5 ],
+            [ 2, 4, 6, 8,  10 ],
+            [ 3, 6, 9, 12, 15 ],
+           ]
+
+    worksheet.write('A1', data)
+
+    chart.add_series(:values => '=Sheet1!$A$1:$A$5')
+    chart.add_series(:values => '=Sheet1!$B$1:$B$5')
+    chart.add_series(:values => '=Sheet1!$C$1:$C$5')
+
+    chart.set_x_axis(
+                     :major_gridlines => {
+                       :visible => 1,
+                       :line    => {
+                         :color     => 'red',
+                         :width     => 0.5,
+                         :dash_type => 'square_dot'
+                       }
+                     },
+                     :minor_gridlines => {
+                       :visible => 1,
+                       :line    => { :color => 'yellow' }
+                     }
+                     )
+    chart.set_y_axis(
+                     :major_gridlines => {
+                       :visible => 1,
+                       :line    => {
+                         :width     => 1.25,
+                         :dash_type => 'dash'
+                       }
+                     },
+                     :minor_gridlines => {
+                       :visible => 1,
+                       :line    => { :color => '#00B050' }
+                     }
+                     )
+
+    worksheet.insert_chart('E9',  chart)
+
+    workbook.close
+    compare_xlsx_for_regression(
+                                File.join(@regression_output, @xlsx),
+                                @xlsx
+                                )
+  end
+end
