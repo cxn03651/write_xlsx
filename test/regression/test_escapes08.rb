2009850ea6db5e3d174890663bbd09a5a894b3f2
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2013-02-19 22:49:36 +0900

    test for bug fix escaping of special character in URLs to Worksheet#write_url.

---
+++ test/regression/test_escapes08.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRegressionEscapes08 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_chart_escapes08
+    @xlsx = 'escapes08.xlsx'
+    workbook   = WriteXLSX.new(@xlsx)
+    worksheet  = workbook.add_worksheet
+
+    # Test an already escaped string.
+    worksheet.write_url(
+                        'A1',
+                        'http://example.com/%5b0%5d', 'http://example.com/[0]'
+                        )
+
+    workbook.close
+    compare_xlsx_for_regression(
+                                File.join(@regression_output, @xlsx),
+                                @xlsx
+                                )
+  end
+end
