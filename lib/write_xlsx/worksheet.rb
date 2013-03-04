3305885a3904a9b2f9c93a835ade88d9ea2972dd
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2013-03-04 09:22:22 +0900

    worksheet.rb : modify rdoc in add_sparkline and insert_button

--- lib/write_xlsx/worksheet.rb
+++ lib/write_xlsx/worksheet.rb
@@ -34,6 +34,7 @@ module Writexlsx
   # * insert_image
   # * insert_chart
   # * insert_shape
+  # * insert_button
   # * data_validation
   # * conditional_formatting
   # * add_sparkline
@@ -4008,7 +4009,265 @@ module Writexlsx
     end
 
     #
+    # :call-seq:
+    #    add_sparkline(properties)
+    #
     # Add sparklines to the worksheet.
+    #
+    # Sparklines are a feature of Excel 2010+ which allows you to add small
+    # charts to worksheet cells. These are useful for showing visual trends
+    # in data in a compact format.
+    #
+    # In WriteXLSX Sparklines can be added to cells using the add_sparkline()
+    # worksheet method:
+    #
+    #     worksheet.add_sparkline(
+    #         {
+    #             :location => 'F2',
+    #             :range    => 'Sheet1!A2:E2',
+    #             :type     => 'column',
+    #             :style    => 12
+    #         }
+    #     )
+    #
+    # Note: Sparklines are a feature of Excel 2010+ only. You can write them
+    # to an XLSX file that can be read by Excel 2007 but they won't be
+    # displayed.
+    #
+    # The add_sparkline() worksheet method is used to add sparklines to a
+    # cell or a range of cells.
+    #
+    # The parameters to add_sparkline() must be passed in a hash.
+    # The main sparkline parameters are:
+    #
+    #     :location        (required)
+    #     :range           (required)
+    #     :type
+    #     :style
+    #
+    #     :markers
+    #     :negative_points
+    #     :axis
+    #     :reverse
+    # Other, less commonly used parameters are:
+    #
+    #     :high_point
+    #     :low_point
+    #     :first_point
+    #     :last_point
+    #     :max
+    #     :min
+    #     :empty_cells
+    #     :show_hidden
+    #     :date_axis
+    #     :weight
+    #
+    #     :series_color
+    #     :negative_color
+    #     :markers_color
+    #     :first_color
+    #     :last_color
+    #     :high_color
+    #     :low_color
+    #
+    # These parameters are explained in the sections below:
+    #
+    # ===:location
+    #
+    # This is the cell where the sparkline will be displayed:
+    #
+    #     location => 'F1'
+    # The location should be a single cell. (For multiple cells see "Grouped Sparklines" below).
+    #
+    # To specify the location in row-column notation use the xl_rowcol_to_cell() function from the Excel::Writer::XLSX::Utility module.
+    #
+    #     use Excel::Writer::XLSX::Utility ':rowcol';
+    #     ...
+    #     location => xl_rowcol_to_cell( 0, 5 ), # F1
+    # range
+    #
+    # This specifies the cell data range that the sparkline will plot:
+    #
+    #     $worksheet->add_sparkline(
+    #         {
+    #             location => 'F1',
+    #             range    => 'A1:E1',
+    #         }
+    #     );
+    # The range should be a 2D array. (For 3D arrays of cells see "Grouped Sparklines" below).
+    #
+    # If range is not on the same worksheet you can specify its location using the usual Excel notation:
+    #
+    #             range => 'Sheet1!A1:E1',
+    # If the worksheet contains spaces or special characters you should quote the worksheet name in the same way that Excel does:
+    #
+    #             range => q('Monthly Data'!A1:E1),
+    # To specify the location in row-column notation use the xl_range() or xl_range_formula() functions from the Excel::Writer::XLSX::Utility module.
+    #
+    #     use Excel::Writer::XLSX::Utility ':rowcol';
+    #     ...
+    #     range => xl_range( 1, 1,  0, 4 ),                   # 'A1:E1'
+    #     range => xl_range_formula( 'Sheet1', 0, 0,  0, 4 ), # 'Sheet1!A2:E2'
+    # type
+    #
+    # Specifies the type of sparkline. There are 3 available sparkline types:
+    #
+    #     line    (default)
+    #     column
+    #     win_loss
+    # For example:
+    #
+    #     {
+    #         location => 'F1',
+    #         range    => 'A1:E1',
+    #         type     => 'column',
+    #     }
+    # style
+    #
+    # Excel provides 36 built-in Sparkline styles in 6 groups of 6. The style parameter can be used to replicate these and should be a corresponding number from 1 .. 36.
+    #
+    #     {
+    #         location => 'A14',
+    #         range    => 'Sheet2!A2:J2',
+    #         style    => 3,
+    #     }
+    # The style number starts in the top left of the style grid and runs left to right. The default style is 1. It is possible to override colour elements of the sparklines using the *_color parameters below.
+    #
+    # markers
+    #
+    # Turn on the markers for line style sparklines.
+    #
+    #     {
+    #         location => 'A6',
+    #         range    => 'Sheet2!A1:J1',
+    #         markers  => 1,
+    #     }
+    # Markers aren't shown in Excel for column and win_loss sparklines.
+    #
+    # negative_points
+    #
+    # Highlight negative values in a sparkline range. This is usually required with win_loss sparklines.
+    #
+    #     {
+    #         location        => 'A21',
+    #         range           => 'Sheet2!A3:J3',
+    #         type            => 'win_loss',
+    #         negative_points => 1,
+    #     }
+    # axis
+    #
+    # Display a horizontal axis in the sparkline:
+    #
+    #     {
+    #         location => 'A10',
+    #         range    => 'Sheet2!A1:J1',
+    #         axis     => 1,
+    #     }
+    # reverse
+    #
+    # Plot the data from right-to-left instead of the default left-to-right:
+    #
+    #     {
+    #         location => 'A24',
+    #         range    => 'Sheet2!A4:J4',
+    #         type     => 'column',
+    #         reverse  => 1,
+    #     }
+    # weight
+    #
+    # Adjust the default line weight (thickness) for line style sparklines.
+    #
+    #      weight => 0.25,
+    # The weight value should be one of the following values allowed by Excel:
+    #
+    #     0.25  0.5   0.75
+    #     1     1.25
+    #     2.25
+    #     3
+    #     4.25
+    #     6
+    # high_point, low_point, first_point, last_point
+    #
+    # Highlight points in a sparkline range.
+    #
+    #         high_point  => 1,
+    #         low_point   => 1,
+    #         first_point => 1,
+    #         last_point  => 1,
+    # max, min
+    #
+    # Specify the maximum and minimum vertical axis values:
+    #
+    #         max         => 0.5,
+    #         min         => -0.5,
+    # As a special case you can set the maximum and minimum to be for a group of sparklines rather than one:
+    #
+    #         max         => 'group',
+    # See "Grouped Sparklines" below.
+    #
+    # empty_cells
+    #
+    # Define how empty cells are handled in a sparkline.
+    #
+    #     empty_cells => 'zero',
+    # The available options are:
+    #
+    #     gaps   : show empty cells as gaps (the default).
+    #     zero   : plot empty cells as 0.
+    #     connect: Connect points with a line ("line" type  sparklines only).
+    # show_hidden
+    #
+    # Plot data in hidden rows and columns:
+    #
+    #     show_hidden => 1,
+    # Note, this option is off by default.
+    #
+    # date_axis
+    #
+    # Specify an alternative date axis for the sparkline. This is useful if the data being plotted isn't at fixed width intervals:
+    #
+    #     {
+    #         location  => 'F3',
+    #         range     => 'A3:E3',
+    #         date_axis => 'A4:E4',
+    #     }
+    # The number of cells in the date range should correspond to the number of cells in the data range.
+    #
+    # series_color
+    #
+    # It is possible to override the colour of a sparkline style using the following parameters:
+    #
+    #     series_color
+    #     negative_color
+    #     markers_color
+    #     first_color
+    #     last_color
+    #     high_color
+    #     low_color
+    # The color should be specified as a HTML style #rrggbb hex value:
+    #
+    #     {
+    #         location     => 'A18',
+    #         range        => 'Sheet2!A2:J2',
+    #         type         => 'column',
+    #         series_color => '#E965E0',
+    #     }
+    # Grouped Sparklines
+    #
+    # The add_sparkline() worksheet method can be used multiple times to write as many sparklines as are required in a worksheet.
+    #
+    # However, it is sometimes necessary to group contiguous sparklines so that changes that are applied to one are applied to all. In Excel this is achieved by selecting a 3D range of cells for the data range and a 2D range of cells for the location.
+    #
+    # In Excel::Writer::XLSX, you can simulate this by passing an array refs of values to location and range:
+    #
+    #     {
+    #         location => [ 'A27',          'A28',          'A29'          ],
+    #         range    => [ 'Sheet2!A5:J5', 'Sheet2!A6:J6', 'Sheet2!A7:J7' ],
+    #         markers  => 1,
+    #     }
+    # Sparkline examples
+    #
+    # See the sparklines1.pl and sparklines2.pl example programs in the examples directory of the distro.
     #
     # The add_sparkline worksheet method is used to add sparklines to a cell or a range of cells.
     #
@@ -4135,7 +4394,87 @@ module Writexlsx
     end
 
     #
-    # Insert a button form object into the worksheet.
+    # :call-seq:
+    #   insert_button(row, col, properties)
+    #
+    # The insert_button() method can be used to insert an Excel form button
+    # into a worksheet.
+    #
+    # This method is generally only useful when used in conjunction with
+    # the Workbook add_vba_project() method to tie the button to a macro
+    # from an embedded VBA project:
+    #
+    #     workbook  = WriteXLSX.new('file.xlsm')
+    #     ...
+    #     workbook.add_vba_project('./vbaProject.bin')
+    #
+    #     worksheet.insert_button('C2', { :macro => 'my_macro' } )
+    #
+    # The properties of the button that can be set are:
+    #
+    #     :macro
+    #     :caption
+    #     :width
+    #     :height
+    #     :x_scale
+    #     :y_scale
+    #     :x_offset
+    #     :y_offset
+    #
+    # === Option: macro
+    # This option is used to set the macro that the button will invoke when
+    # the user clicks on it. The macro should be included using the
+    # Workbook#add_vba_project() method shown above.
+    #
+    #     worksheet.insert_button('C2', { :macro => 'my_macro' } )
+    #
+    # The default macro is +ButtonX_Click+ where X is the button number.
+    #
+    # ===Option: caption
+    # This option is used to set the caption on the button. The default is
+    # <tt>Button X</tt> where X is the button number.
+    #
+    #     worksheet.insert_button('C2', { :macro => 'my_macro', :caption => 'Hello' })
+    #
+    # ===Option: width
+    # This option is used to set the width of the button in pixels.
+    #
+    #     worksheet.insert_button('C2', { :macro => 'my_macro', :width => 128 })
+    #
+    # The default button width is 64 pixels which is the width of a default cell.
+    #
+    # ===Option: height
+    # This option is used to set the height of the button in pixels.
+    #
+    #     worksheet.insert_button('C2', { :macro => 'my_macro', :height => 40 })
+    #
+    # The default button height is 20 pixels which is the height of a default cell.
+    #
+    # ===Option: x_scale
+    # This option is used to set the width of the button as a factor of the
+    # default width.
+    #
+    #     worksheet.insert_button('C2', { :macro => 'my_macro', :x_scale => 2.0 })
+    #
+    # ===Option: y_scale
+    # This option is used to set the height of the button as a factor of the
+    # default height.
+    #
+    #     worksheet.insert_button('C2', { :macro => 'my_macro', y_:scale => 2.0 } )
+    #
+    # ===Option: x_offset
+    # This option is used to change the x offset, in pixels, of a button
+    # within a cell:
+    #
+    #     worksheet.insert_button('C2', { :macro => 'my_macro', :x_offset => 2 })
+    #
+    # ===Option: y_offset
+    # This option is used to change the y offset, in pixels, of a comment
+    # within a cell.
+    #
+    # Note: Button is the only Excel form element that is available in
+    # WriteXLSX. Form elements represent a lot of work to implement and the
+    # underlying VML syntax isn't very much fun.
     #
     def insert_button(*args)
       @buttons_array << button_params(*(row_col_notation(args)))
