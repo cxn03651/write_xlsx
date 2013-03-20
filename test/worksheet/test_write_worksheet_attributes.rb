c0df2ef272cd6942990eb8c3ae8297e83685adfa
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2013-03-20 19:54:31 +0900

    Replace test_write_worksheet with test_write_worksheet_attributes.

---
+++ test/worksheet/test_write_worksheet_attributes.rb
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+require 'write_xlsx/workbook'
+require 'write_xlsx/worksheet'
+require 'stringio'
+
+class TestWriteWorksheetAttributes < Test::Unit::TestCase
+  def setup
+    @workbook = WriteXLSX.new(StringIO.new)
+    @worksheet = @workbook.add_worksheet('')
+  end
+
+  def test_write_worksheet_attributes
+    result = @worksheet.__send__('write_worksheet_attributes')
+    expected = [
+                'xmlns', "http://schemas.openxmlformats.org/spreadsheetml/2006/main",
+                'xmlns:r', "http://schemas.openxmlformats.org/officeDocument/2006/relationships"
+                ]
+    assert_equal(expected, result)
+  end
+end
