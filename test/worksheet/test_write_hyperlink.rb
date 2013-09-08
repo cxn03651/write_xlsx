ef625fefa438d86297f5470b5b072852db8172c5
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2013-09-08 18:00:27 +0900

    add InternalHyperlink and ExternalHyperlink class.

--- test/worksheet/test_write_hyperlink.rb
+++ test/worksheet/test_write_hyperlink.rb
@@ -10,7 +10,7 @@ class TestWriteHyperlink < Test::Unit::TestCase
   end
 
   def test_write_hyperlink_external
-    hyperlink = Writexlsx::Worksheet::Hyperlink.new('')
+    hyperlink = Writexlsx::Worksheet::Hyperlink.factory('')
     @worksheet.__send__('write_hyperlink_external', hyperlink, 0, 0, 1)
     result = @worksheet.instance_variable_get(:@writer).string
     expected = '<hyperlink ref="A1" r:id="rId1"/>'
@@ -18,7 +18,7 @@ class TestWriteHyperlink < Test::Unit::TestCase
   end
 
   def test_write_hyperlink_internal_sheet2
-    hyperlink = Writexlsx::Worksheet::Hyperlink.new('internal:Sheet2!A1', 'Sheet2!A1')
+    hyperlink = Writexlsx::Worksheet::Hyperlink.factory('internal:Sheet2!A1', 'Sheet2!A1')
     @worksheet.__send__('write_hyperlink_internal', hyperlink, 0, 0)
     result = @worksheet.instance_variable_get(:@writer).string
     expected = '<hyperlink ref="A1" location="Sheet2!A1" display="Sheet2!A1"/>'
@@ -26,7 +26,7 @@ class TestWriteHyperlink < Test::Unit::TestCase
   end
 
   def test_write_hyperlink_internal_quoted_sheet
-    hyperlink = Writexlsx::Worksheet::Hyperlink.new("internal:'Data Sheet'!D5", "'Data Sheet'!D5")
+    hyperlink = Writexlsx::Worksheet::Hyperlink.factory("internal:'Data Sheet'!D5", "'Data Sheet'!D5")
     @worksheet.__send__('write_hyperlink_internal', hyperlink, 4, 0)
     result = @worksheet.instance_variable_get(:@writer).string
     expected = %q{<hyperlink ref="A5" location="'Data Sheet'!D5" display="'Data Sheet'!D5"/>}
@@ -34,7 +34,7 @@ class TestWriteHyperlink < Test::Unit::TestCase
   end
 
   def test_write_hyperlink_internal_tooltip
-    hyperlink = Writexlsx::Worksheet::Hyperlink.new('internal:Sheet2!A1', 'Sheet2!A1')
+    hyperlink = Writexlsx::Worksheet::Hyperlink.factory('internal:Sheet2!A1', 'Sheet2!A1')
     hyperlink.tip = 'Screen Tip 1'
     @worksheet.__send__('write_hyperlink_internal', hyperlink, 17, 0)
     result = @worksheet.instance_variable_get(:@writer).string
