87e4adcc58477a68b3f847312a6dae8e4c289fcc
Hideo NAKAMURA <cxn03651@Air11.local>
2014-05-11 07:07:15 +0900

    Added testcase for issue https://github.com/jmcnamara/excel-writer-xlsx/issues/90

---
+++ test/regression/test_merge_cells01.rb
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRegressionMergeCells01 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_merge_cells01
+    @xlsx = 'merge_cells01.xlsx'
+    workbook  = WriteXLSX.new(@xlsx)
+    worksheet = workbook.add_worksheet
+    format    = workbook.add_format(:align => 'center')
+
+    worksheet.set_selection('A4')
+
+    worksheet.merge_range('A1:A2', 'col1', format)
+    worksheet.merge_range('B1:B2', 'col2', format)
+    worksheet.merge_range('C1:C2', 'col3', format)
+    worksheet.merge_range('D1:D2', 'col4', format)
+
+    workbook.close
+    compare_xlsx_for_regression(File.join(@regression_output, @xlsx), @xlsx)
+  end
+end
