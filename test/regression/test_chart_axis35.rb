0682bc41cad17559e8448d361479583c069f58cf
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2014-12-04 15:14:12 +0900

    tests for v0.81.00

---
+++ test/regression/test_chart_axis35.rb
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRegressionChartAxis35 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_chart_axis35
+    @xlsx = 'chart_axis35.xlsx'
+    workbook    = WriteXLSX.new(@xlsx)
+    worksheet   = workbook.add_worksheet
+    chart       = workbook.add_chart(:type => 'column', :embedded => 1)
+
+    # For testing, copy the randomly generated axis ids in the target xlsx file.
+    chart.instance_variable_set(:@axis_ids, [63008128, 62522496])
+    data = [
+            [ 1, 2, 3, 4,  5 ],
+            [ 2, 4, 6, 8,  10 ],
+            [ 3, 6, 9, 12, 15 ]
+           ]
+
+    worksheet.write('A1', data)
+
+    chart.add_series(:values => '=Sheet1!$A$1:$A$5')
+    chart.add_series(:values => '=Sheet1!$B$1:$B$5')
+    chart.add_series(:values => '=Sheet1!$C$1:$C$5')
+
+    chart.set_y_axis(:line => {:none => 1})
+
+    worksheet.insert_chart('E9', chart)
+
+    workbook.close
+    compare_xlsx_for_regression(File.join(@regression_output, @xlsx),
+                                @xlsx,
+                                nil,
+                                { 'xl/charts/chart1.xml' => ['<c:pageMargins'] }
+                                )
+  end
+end
