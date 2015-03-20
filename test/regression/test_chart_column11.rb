8a645c24561533ab1054c46aa75a69492b601e49
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2015-03-20 22:34:37 +0900

    test_chart_column11 and test_chart_column12 added.

---
+++ test/regression/test_chart_column11.rb
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRegressionChartColumn11 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_chart_column11
+    @xlsx = 'chart_column11.xlsx'
+    workbook  = WriteXLSX.new(@xlsx)
+    worksheet = workbook.add_worksheet
+    chart     = workbook.add_chart(:type => 'column', :embedded => 1)
+
+    # For testing, copy the randomly generated axis ids in the target xlsx file.
+    chart.instance_variable_set(:@axis_ids, [46847488, 46849408])
+
+    data = [
+      [1, 2, 3,  4,  5],
+      [2, 4, 6,  8, 10],
+      [3, 6, 9, 12, 15]
+    ]
+
+    worksheet.write('A1', data)
+
+    chart.add_series(:values => '=Sheet1!$A$1:$A$5')
+    chart.add_series(:values => '=Sheet1!$B$1:$B$5')
+    chart.add_series(:values => '=Sheet1!$C$1:$C$5')
+
+    chart.set_style(1)
+
+    worksheet.insert_chart('E9', chart)
+
+    workbook.close
+    compare_xlsx_for_regression(File.join(@regression_output, @xlsx),
+                                @xlsx,
+                                nil,
+                                nil
+                                )
+  end
+end
