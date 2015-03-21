a20ec878b0b14c9d900e9f115a40fdf8cc7b1976
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2015-03-22 02:56:43 +0900

    add test_quote_name04.rb

---
+++ test/regression/test_quote_name04.rb
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRegressionQuoteName04 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_quote_name04
+    @xlsx = 'quote_name04.xlsx'
+    workbook  = WriteXLSX.new(@xlsx)
+
+    data = [
+            [1, 2, 3,  4,  5],
+            [2, 4, 6,  8, 10],
+            [3, 6, 9, 12, 15]
+           ]
+
+    # Test quoted/non-quoted sheet names.
+    worksheet = workbook.add_worksheet('Sheet 1')
+    chart = workbook.add_chart(:type => 'pie', :embedded => 1)
+
+    worksheet.write( 'A1', data )
+    chart.add_series(
+      :values => ['Sheet 1', 0, 4, 0, 0],
+      :name   => ['Sheet 1', 0, 0]
+    )
+
+    chart.set_title(:name => 'Foo')
+
+    worksheet.insert_chart('E9', chart)
+
+    workbook.close
+    compare_xlsx_for_regression(File.join(@regression_output, @xlsx), @xlsx)
+  end
+end
