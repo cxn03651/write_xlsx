2a8307eaec5751cdbd3e57ff70c45c5243230b34
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2015-04-11 22:32:14 +0900

    Fix for overwriting user options in charts
    Fix to copy user defined chart properties, such as trendlines,
    so that they aren't overwritten.

---
+++ test/regression/test_chart_format20.rb
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRegressionChartFormat20 < Test::Unit::TestCase
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    File.delete(@xlsx) if File.exist?(@xlsx)
+  end
+
+  def test_chart_format20
+    @xlsx = 'chart_format20.xlsx'
+    workbook    = WriteXLSX.new(@xlsx)
+    worksheet   = workbook.add_worksheet
+    chart1      = workbook.add_chart(:type => 'line', :embedded => 1)
+    chart2      = workbook.add_chart(:type => 'line', :embedded => 1)
+
+    # For testing, copy the randomly generated axis ids in the target xlsx file.
+    chart1.instance_variable_set(:@axis_ids, [80553856, 80555392])
+    chart2.instance_variable_set(:@axis_ids, [84583936, 84585856])
+
+    trend = {
+      :type => 'linear',
+      :line => {:color => 'red', :dash_type => 'dash'}
+    }
+
+    data = [
+            [ 1, 2, 3, 4,  5 ],
+            [ 2, 4, 6, 8,  10 ],
+            [ 3, 6, 9, 12, 15 ]
+           ]
+
+    worksheet.write('A1', data)
+
+    chart1.add_series(
+      :values    => '=Sheet1!$B$1:$B$5',
+      :trendline => trend
+    )
+    chart1.add_series(:values => '=Sheet1!$C$1:$C$5')
+    chart2.add_series(
+      :values    => '=Sheet1!$B$1:$B$5',
+      :trendline => trend
+    )
+    chart2.add_series(:values => '=Sheet1!$C$1:$C$5')
+
+    worksheet.insert_chart('E9',  chart1)
+    worksheet.insert_chart('E25', chart2)
+
+    workbook.close
+    compare_xlsx_for_regression(
+                                File.join(@regression_output, @xlsx), @xlsx)
+  end
+end
