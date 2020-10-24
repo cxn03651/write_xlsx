3bb79bbcd1938fbac228b20d80847282cbc3f423
Hideo NAKAMURA <nakamura.hideo@gmail.com>
2020-10-25 08:00:47 +0900

    add examples/update_range_format_with_params.rb

---
+++ examples/update_range_format_with_params.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+#######################################################################
+#
+# A simple example of how to use the write_xlsx gem to
+# update format of the range of cells.
+#
+
+require 'write_xlsx'
+
+workbook = WriteXLSX.new('update_range_format_with_params.xlsx')
+worksheet = workbook.add_worksheet
+
+common_format = workbook.add_format(align: 'center', border: 1)
+
+table_contents = [
+  ['Table', 'Header', 'Contents'],
+  ['table', 'body', 'contents'],
+  ['table', 'body', 'contents'],
+  ['table', 'body', 'contents']
+]
+
+worksheet.write_col(0, 0, table_contents, common_format)
+worksheet.update_range_format_with_params(
+  0, 0, 0, 2,
+  bold: 1, top: 2, bottom: 2, bg_color: 31
+)
+worksheet.update_range_format_with_params(0, 0, 3, 0, left: 2)
+worksheet.update_range_format_with_params(0, 2, 3, 2, right: 2)
+worksheet.update_range_format_with_params(3, 0, 3, 2, bottom: 2)
+
+workbook.close
