5eac72adbee1170688a7d24ba9034e71683d368b
Hideo NAKAMURA <nakamura.hideo@nihon-u.ac.jp>
2020-12-27 08:59:35 +0900

    Added test for set_center_across().

---
+++ test/regression/test_format14.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRegressionFormat14 < Minitest::Test
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    @tempfile.close(true)
+  end
+
+  def test_format14
+    @xlsx = 'format14.xlsx'
+    workbook  = WriteXLSX.new(@io)
+    worksheet = workbook.add_worksheet
+    center    = workbook.add_format
+
+    center.set_center_across
+
+    worksheet.write('A1', 'foo', center)
+
+    workbook.close
+    compare_for_regression
+  end
+end
