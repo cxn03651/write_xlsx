a8a25bd5ebf03a117a7d639ac7e1784b3619f1b8
Hideo NAKAMURA <nakamura.hideo@nihon-u.ac.jp>
2020-12-28 20:29:52 +0900

    Added test for full-width number strings treated as numbers.

---
+++ test/regression/test_utf8_11.rb
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRegressionUtf8_11 < Minitest::Test
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    @tempfile.close(true)
+  end
+
+  def test_utf8_11
+    @xlsx = 'utf8_11.xlsx'
+    workbook  = WriteXLSX.new(@io)
+    worksheet = workbook.add_worksheet
+
+    worksheet.write( 'A1', '１２３４５' )
+
+    workbook.close
+    compare_for_regression
+  end
+end
