ed9fa412a6bb891cd50f7cd20d8778e03cb9f7f9
Hideo NAKAMURA <nakamura.hideo@nihon-u.ac.jp>
2020-12-30 20:30:10 +0900

    Added table test for single column header.

---
+++ test/regression/test_table21.rb
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+require 'helper'
+
+class TestRegressionTable21 < Minitest::Test
+  def setup
+    setup_dir_var
+  end
+
+  def teardown
+    @tempfile.close(true) if @tempfile
+  end
+
+  def test_table21
+    @xlsx = 'table21.xlsx'
+    workbook  = WriteXLSX.new(@io)
+    worksheet = workbook.add_worksheet
+
+    worksheet.write('A1', 'Column')
+
+    # Set the column width to match the taget worksheet.
+    worksheet.set_column('C:D', 10.288)
+
+    # Add the table.
+    worksheet.add_table(
+      'C3:D13',
+      {
+        :columns => [
+          { :header => "Column" }
+        ]
+      }
+    )
+
+    workbook.close
+    compare_for_regression
+  end
+end
