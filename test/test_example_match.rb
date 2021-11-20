cabc5d7eed3c7f811ab790a3b1b9473f0673ca79
Hideo NAKAMURA <nakamura.hideo@gmail.com>
2021-11-20 12:39:01 +0900

    add example ignore_errors.rb

--- test/test_example_match.rb
+++ test/test_example_match.rb
@@ -3292,6 +3292,37 @@ EOS
     compare_xlsx(File.join(@perl_output, @xlsx), @tempfile.path)
   end
 
+  def test_ignore_errors
+    @xlsx = 'ignore_errors.xlsx'
+    workbook = WriteXLSX.new(@io)
+    worksheet = workbook.add_worksheet
+
+    # Write strings that looks like numbers. This will cause an Excel warning.
+    worksheet.write_string('C2', '123')
+    worksheet.write_string('C3', '123')
+
+    # Write a divide by zero formula. This will also cause an Excel warning.
+    worksheet.write_formula('C5', '=1/0')
+    worksheet.write_formula('C6', '=1/0')
+
+    # Turn off some of the warnings:
+    worksheet.ignore_errors(
+      :number_stored_as_text => 'C3',
+      :eval_error => 'C6'
+    )
+
+    # Write some descriptions for the cells and make the column wider for clarity.
+    worksheet.set_column('B:B', 16)
+    worksheet.write('B2', 'Warning:')
+    worksheet.write('B3', 'Warning turned off:')
+    worksheet.write('B5', 'Warning:')
+    worksheet.write('B6', 'Warning turned off:')
+
+    workbook.close
+    store_to_tempfile
+    compare_xlsx(File.join(@perl_output, @xlsx), @tempfile.path)
+  end
+
   def test_merge1
     @xlsx = 'merge1.xlsx'
     # Create a new workbook and add a worksheet
