b60f8c89056d7762f8c5af2228467e2bdc2744be
Axel Miguez <axel.ataides@gmail.com>
2022-07-08 23:07:45 -0300

    Dealing with NaN values

--- lib/write_xlsx/worksheet.rb
+++ lib/write_xlsx/worksheet.rb
@@ -2044,6 +2044,8 @@ module Writexlsx
     # Write the cell value <v> element.
     #
     def write_cell_value(value = '') #:nodoc:
+      return write_cell_formula('=NA()') if !value.nil? && value.is_a?(Float) && value.nan?
+
       value ||= ''
       value = value.to_i if value == value.to_i
       @writer.data_element('v', value)
