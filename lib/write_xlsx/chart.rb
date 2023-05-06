be2424033e9cad4822a845ed1f1421bc6ca6bc8a
Hideo NAKAMURA <nakamura.hideo@gmail.com>
2023-05-06 11:26:00 +0900

    Minor chart style range fix.

--- lib/write_xlsx/chart.rb
+++ lib/write_xlsx/chart.rb
@@ -371,7 +371,7 @@ module Writexlsx
     # Set on of the 42 built-in Excel chart styles. The default style is 2.
     #
     def set_style(style_id = 2)
-      style_id = 2 if style_id < 0 || style_id > 48
+      style_id = 2 if style_id < 1 || style_id > 48
       @style_id = style_id
     end
 
